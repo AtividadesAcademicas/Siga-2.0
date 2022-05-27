921b2ee613078dc47b1b5c094c40f0994c554886
Gustavo N. S <gustavo.ns@outlook.com.br>
2022-05-27 10:32:05 -0300

    :pencil: Added Dark/Light Theme

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,17 +1,37 @@
-/*==== SHOW MENU =====*/
 /*=============== SHOW MENU ===============*/
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById('nav-toggle'),
-        nav = document.getElementById('nav-menu'),
-        toggleIcon = document.getElementById('toggle-icon')
+        nav = document.getElementById('nav-menu')
 
     //Valida se as variáveis existem, variáveis essas que são
     if (toggle && nav) {
         //Tendo a existência das variáveis, podemos adicionar a classe 'show-menu' a barra de navegação
         toggle.addEventListener('click', () => {
             nav.classList.toggle('show-menu')
-            toggleIcon.classList.toggle('bi-x-lg')
+            toggle.classList.toggle('bi-x-lg')
         })
     }
 }
 showMenu('nav-toggle', 'nav-menu')
+
+/*=============== DARK THEME ===============*/
+const themeButton = document.getElementById('change-theme')
+const iconTheme = document.getElementById('change-theme-icon')
+const darkTheme = 'dark-theme'
+const changeIcon = 'bi-toggle-on'
+
+//Obtemos o tema atual que a interface tem validando a classe 'dark-theme'
+const getCurrentTheme = () =>
+    document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+
+const getCurrentIcon = () =>
+    themeButton.classList.contains(changeIcon) ? 'bi-toggle-off' : 'bi-toggle-on'
+
+//Ativar / Desativar o tema com o botão
+themeButton.addEventListener('click', () => {
+    //Adiciona ou remove a classe 'dark-theme' ao body
+    document.body.classList.toggle('dark-theme')
+
+    //Adiciona ou remove a classe 'bi-toggle-on' ao ícone
+    iconTheme.classList.toggle(changeIcon)
+})
