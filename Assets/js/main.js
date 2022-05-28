67b13f6314143160d6629c57e1c968a9bd1d7772
Gustavo Nascimento Souza <gustavo.ns@outlook.com.br>
2022-05-27 23:41:52 -0300

    :pencil: Added Consultas & Agenda (their both more responsive), Modify Navigation Bar

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -35,3 +35,30 @@ themeButton.addEventListener('click', () => {
     //Adiciona ou remove a classe 'bi-toggle-on' ao ícone
     iconTheme.classList.toggle(changeIcon)
 })
+
+/*=============== PROGRESS BAR ===============*/
+var i = 1;
+while (i <= 6) {
+    let progressBar = document.querySelector(`.circular-progress-${i}`)
+    let valueContainer = document.querySelector(`.value-container-${i}`)
+
+    let progressValue = 0;
+    let progressEndValue = valueContainer.textContent;
+    let speed = 10;
+
+    let progress = setInterval(() => {
+        progressValue++
+
+        valueContainer.textContent = `${progressValue}%`
+        progressBar.style.background = `conic-gradient(
+        var(--red) ${progressValue * 3.6}deg,
+        var(--brand) ${progressValue * 3.6}deg
+    )`
+
+        if (progressValue == progressEndValue) {
+            clearInterval(progress)
+        }
+    }, speed)
+
+    i++
+}
